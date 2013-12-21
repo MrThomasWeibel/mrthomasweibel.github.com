1672caef02008738bea506d4ff39f1d22c70683d
MrThomasWeibel <weibel@outlook.com>
2013-12-21 18:55:07 +0100

    Version 1.00

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="da">
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>8.X - Lektier</title>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script type="text/javascript" src="login.js"></script>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body>
+	    <div class="container">
+	    	<h1 class="title" href="#">8.X Lektier</h1>
+	    	<a class="login link" href="#">Log ud</a>
+	    	<p>Her kan man tilføje, redigere og slette lektier!</p>
+		</div>
+	</body>
+</html>
